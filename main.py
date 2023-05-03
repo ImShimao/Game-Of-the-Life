e63314112ca7019b08e543ab93b6b686fd6aae77
Shimao <113797594+ImShimao@users.noreply.github.com>
2023-05-03 18:39:50 +0200

    Shimao

---
+++ main.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+import gameoflife
+import pyxel
+
+class GameOfLifeGUI:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.geometry('1920x1080')
+        self.root.title('Game of the Life')
+
+        title_label = tk.Label(self.root, text='Game of Life', font=('Arial', 60))
+        title_label.pack(pady=100)
+
+        start_button = tk.Button(self.root, text='Start', font=('Arial', 30), command=self.start_game)
+        start_button.pack(pady=50)
+
+        self.root.mainloop()
+
+    def start_game(self):
+        self.root.destroy()
+
+        pyxel.init(800, 800, title='Game of Life', fps=15)
+        game = gameoflife.GameOfLife()
+        pyxel.run(game.update, game.draw)
+
+if __name__ == '__main__':
+    GameOfLifeGUI()
